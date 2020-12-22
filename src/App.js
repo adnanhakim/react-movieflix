7269d02a5147ca2382d32d2e700cd4dd74d09032
Adnan Hakim <adnanhakim007@gmail.com>
2020-12-22 21:03:55 +0530

    Rows added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Row from './components/Row';
+import requests from './requests';
 
 function App() {
    return (
       <div className="App">
          <h1>Hi mmmm</h1>
+         <Row
+            title="Netflix Originals"
+            fetchUrl={requests.fetchNetflixOriginals}
+            isLargeRow
+         />
+         <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+         <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+         <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+         <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+         <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+         <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+         <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
       </div>
    );
 }
---
+++ src/components/Row.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import './Row.css';
+import axios from '../axios';
+const imageUrl = 'https://image.tmdb.org/t/p/original';
+
+function Row({ title, fetchUrl, isLargeRow }) {
+   const [movies, setMovies] = useState([]);
+
+   useEffect(() => {
+      async function fetchData() {
+         const request = await axios.get(fetchUrl);
+         setMovies(request.data.results);
+         return request;
+      }
+      fetchData();
+   }, [fetchUrl]);
+
+   return (
+      <div className="row">
+         <h2>{title}</h2>
+
+         <div className="row-posters">
+            {movies.map((movie) => (
+               <img
+                  key={movie.id}
+                  className={`row-poster ${isLargeRow && 'row-poster-large'}`}
+                  src={`${imageUrl}${
+                     isLargeRow ? movie.poster_path : movie.backdrop_path
+                  }`}
+                  alt={movie.name}
+                  title={movie.name}
+               />
+            ))}
+         </div>
+      </div>
+   );
+}
+
+export default Row;
